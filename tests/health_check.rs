ad25e272ff4565df31b4e61ed5541c71f3a83457
gsech <sech.gregory@gmail.com>
2024-03-26 17:56:41 +0100

    fix: removed useless dereferencing.

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -132,7 +132,7 @@ async fn spawn_app() -> TestApp {
 
 pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
     let mut connection =
-        PgConnection::connect(&config.connection_string_without_db().expose_secret())
+        PgConnection::connect(config.connection_string_without_db().expose_secret())
             .await
             .expect("Failed to connect to Postgres");
 
