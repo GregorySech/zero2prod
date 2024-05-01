92240eb99b63b4cda38a1b6a822a4082f43ea9b4
Gregory Sech <sech.gregory@gmail.com>
2024-05-01 21:35:38 +0200

    red: test response of confirmation link.

--- tests/api/subscriptions_confirm.rs
+++ tests/api/subscriptions_confirm.rs
@@ -1,3 +1,6 @@
+use reqwest::Url;
+use wiremock::{matchers::{method, path}, Mock, ResponseTemplate};
+
 use crate::helpers::spawn_app;
 
 #[tokio::test]
@@ -11,4 +14,40 @@ async fn confirmations_without_token_are_rejected_with_a_400() {
         .unwrap();
 
     assert_eq!(response.status().as_u16(), 400, "Confirmation requests without tokens are bad requests!");
+}
+
+#[tokio::test]
+async fn the_link_returned_by_subscribe_returns_a_200_if_called() {
+    let app = spawn_app().await;
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .expect(1)
+        .mount(&app.email_server)
+        .await;
+
+    app.post_subscriptions(body.into()).await;
+
+    let email_request = &app.email_server.received_requests().await.unwrap()[0];
+    let email_body: serde_json::Value = serde_json::from_slice(&email_request.body).unwrap();
+
+    let get_link = |s: &str| {
+        let links: Vec<_> = linkify::LinkFinder::new()
+            .links(s)
+            .filter(|link| *link.kind() == linkify::LinkKind::Url)
+            .collect();
+        assert_eq!(links.len(), 1);
+        links[0].as_str().to_owned()
+    };
+
+    let raw_confirmation_link = &get_link(email_body["HtmlBody"].as_str().unwrap());
+    let confirmation_link = Url::parse(raw_confirmation_link).unwrap();
+    // TODO this should really come from the app configuration...
+    assert_eq!(confirmation_link.host_str().unwrap(), "127.0.0.1", "No random APIs");
+
+    let response = reqwest::get(confirmation_link).await.unwrap();
+
+    assert_eq!(response.status().as_u16(), 200, "Confirmation link should return OK on request.");
 }
