fb7ce352e7f9816775f2c4ecf54b858dbbb42915
Gregory Sech <sech.gregory@gmail.com>
2024-05-02 07:48:34 +0200

    red: after following link user status should be confirmed

--- tests/api/subscriptions_confirm.rs
+++ tests/api/subscriptions_confirm.rs
@@ -1,5 +1,8 @@
 use reqwest::Url;
-use wiremock::{matchers::{method, path}, Mock, ResponseTemplate};
+use wiremock::{
+    matchers::{method, path},
+    Mock, ResponseTemplate,
+};
 
 use crate::helpers::spawn_app;
 
@@ -13,7 +16,11 @@ async fn confirmations_without_token_are_rejected_with_a_400() {
         .await
         .unwrap();
 
-    assert_eq!(response.status().as_u16(), 400, "Confirmation requests without tokens are bad requests!");
+    assert_eq!(
+        response.status().as_u16(),
+        400,
+        "Confirmation requests without tokens are bad requests!"
+    );
 }
 
 #[tokio::test]
@@ -32,13 +39,54 @@ async fn the_link_returned_by_subscribe_returns_a_200_if_called() {
 
     let email_request = &app.email_server.received_requests().await.unwrap()[0];
     let confirmation_links = app.get_confirmation_links(email_request);
-    
 
     assert_eq!(confirmation_links.html, confirmation_links.plain_text);
-    assert_eq!(confirmation_links.html.host_str().unwrap(), app.base_url.host_str().unwrap(), "No random APIs.");
+    assert_eq!(
+        confirmation_links.html.host_str().unwrap(),
+        app.base_url.host_str().unwrap(),
+        "No random APIs."
+    );
 
     let response = reqwest::get(confirmation_links.html).await.unwrap();
 
-    assert_eq!(response.status().as_u16(), 200, "Confirmation link should return OK on request.");
+    assert_eq!(
+        response.status().as_u16(),
+        200,
+        "Confirmation link should return OK on request."
+    );
 }
 
+#[tokio::test]
+async fn clicking_on_the_confirmation_link_confirms_the_user() {
+    let app = spawn_app().await;
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .expect(1)
+        .mount(&app.email_server)
+        .await;
+
+    app.post_subscriptions(body.into()).await;
+    let email_request = &app.email_server.received_requests().await.unwrap()[0];
+    let confirmation_links = app.get_confirmation_links(email_request);
+
+    let response = reqwest::get(confirmation_links.html).await.unwrap();
+
+    assert_eq!(response.status().as_u16(), 200);
+
+    let db_pool = app.db_pool;
+
+    let saved = sqlx::query!("SELECT email, name, status FROM subscriptions")
+        .fetch_one(&db_pool)
+        .await
+        .expect("Failed to fetch saved subscription.");
+
+    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved.name, "le guin");
+    assert_eq!(
+        saved.status, "confirmed",
+        "After following confirmation link user should have confirmed status."
+    );
+}
