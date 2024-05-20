4b676c077e733f4f3a5d8ef90108adeddabfe88d
Gregory Sech <sech.gregory@gmail.com>
2024-05-20 16:49:46 +0200

    red: testing required basic authentication in newsletter endpoint!

--- tests/api/newsletter.rs
+++ tests/api/newsletter.rs
@@ -89,6 +89,27 @@ async fn newsletters_returns_400_for_invalid_data() {
     }
 }
 
+#[tokio::test]
+async fn requests_missing_authorization_are_rejected() {
+    let app = spawn_app().await;
+
+    let response = reqwest::Client::new()
+        .post(&format!("{}/newsletters", &app.address))
+        .json(&serde_json::json!({
+            "title": "Newsletter title",
+            "content": {
+                "test": "plain text",
+                "html": "<p>html</p>"
+            }
+        }))
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert_eq!(401, response.status().as_u16());
+    assert_eq!(r#"Basic realm="publish""#, response.headers()["WWW-Authenticate"]);
+}
+
 async fn create_unconfirmed_subscriber(app: &TestApp) -> ConfirmationLinks {
     let body = "name=gregory&email=example@gmail.com";
 
