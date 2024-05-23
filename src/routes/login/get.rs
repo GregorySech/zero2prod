4dc53b4306ae58e9b2d37dc24fe37540c33c9bad
Gregory Sech <sech.gregory@gmail.com>
2024-05-23 12:07:24 +0200

    fix: htmlescaping the error messages got from the URL. Not sicure tho.

--- src/routes/login/get.rs
+++ src/routes/login/get.rs
@@ -7,7 +7,10 @@ pub struct QueryParams {
 
 pub async fn login_form(query: web::Query<QueryParams>) -> HttpResponse {
     let error_html = match query.0.error {
-        Some(error_message) => format!("<p><i>{error_message}</i></p>"),
+        Some(error_message) => format!(
+            "<p><i>{}</i></p>",
+            htmlescape::encode_minimal(&error_message)
+        ),
         None => "".into(),
     };
 
