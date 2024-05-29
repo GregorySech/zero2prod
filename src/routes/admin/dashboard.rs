c215a0e1a9b210aced534bb3c6f3b0f2f2501b90
Gregory Sech <sech.gregory@gmail.com>
2024-05-29 15:40:18 +0200

    green: redirect to dashboard page after login.

--- src/routes/admin/dashboard.rs
+++ src/routes/admin/dashboard.rs
@@ -1,5 +1,65 @@
-use actix_web::HttpResponse;
+use actix_session::Session;
+use actix_web::{http::header::ContentType, web, HttpResponse};
+use anyhow::Context;
+use sqlx::PgPool;
+use tracing::field::display;
+use uuid::Uuid;
 
-pub async fn admin_dashboard() -> HttpResponse {
-    HttpResponse::Ok().finish()
+fn e500<ErrorType>(e: ErrorType) -> actix_web::Error
+where ErrorType: std::fmt::Debug + std::fmt::Display + 'static
+{
+    actix_web::error::ErrorInternalServerError(e)
+}
+
+#[tracing::instrument(
+    name = "Admin dashboard", 
+    skip(session, pool),
+    fields(username=tracing::field::Empty, user_id=tracing::field::Empty)
+)]
+pub async fn admin_dashboard(
+    session: Session,
+    pool: web::Data<PgPool>,
+) -> Result<HttpResponse, actix_web::Error> {
+
+    let username = if let Some(user_id) = session.get::<Uuid>("user_id").map_err(e500)? {
+        tracing::Span::current().record("user_id", &display(&user_id));
+        get_username(user_id, &pool).await.map_err(e500)?
+    } else {
+        todo!()
+    };
+    tracing::Span::current().record("username", &display(&username));
+    let body = format!(r#"
+    <!DOCTYPE html>
+    <html lang="en">
+        <head>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8">
+        <title>Admin dashboard</title>
+        </head>
+
+        <body>
+            <p>Welcome {username}</p>
+        </body>
+    </html>
+    "#);
+
+    Ok(HttpResponse::Ok()
+    .content_type(ContentType::html())
+    .body(body))
+}
+
+#[tracing::instrument(name = "Get username", skip(pool))]
+async fn get_username(user_id: Uuid, pool: &PgPool) -> Result<String, anyhow::Error> {
+    let row = sqlx::query!(
+        r#"
+        SELECT username
+        FROM users
+        WHERE user_id = $1
+        "#,
+        user_id,
+    )
+    .fetch_one(pool)
+    .await
+    .context("Failed query to retrieve username.")?;
+
+    Ok(row.username)
 }
