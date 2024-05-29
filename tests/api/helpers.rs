e45b41b6bf9e0ea8fa0b7e510eeae7ca12d01fbf
Gregory Sech <sech.gregory@gmail.com>
2024-05-29 16:44:27 +0200

    red: you must be logged in to access the admin dashboard.

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -93,14 +93,15 @@ impl TestApp {
     }
 
     pub async fn get_admin_dashboard_html(&self) -> String {
+        self.get_admin_dashboard().await.text().await.unwrap()
+    }
+
+    pub async fn get_admin_dashboard(&self) -> reqwest::Response {
         self.api_client
             .get(&format!("{}/admin/dashboard", &self.address))
             .send()
             .await
-            .expect("Failed to execute request.")
-            .text()
-            .await
-            .unwrap()
+            .expect("Failed to execute request to admin dashboard.")
     }
 }
 
