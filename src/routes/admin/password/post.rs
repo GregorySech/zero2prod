03e966b97311c1cf57be60661a49611a39266419
Gregory Sech <sech.gregory@gmail.com>
2024-05-31 11:49:46 +0200

    green: user provides correct password to change it!

--- src/routes/admin/password/post.rs
+++ src/routes/admin/password/post.rs
@@ -1,10 +1,10 @@
 use actix_web::{web, HttpResponse};
 use actix_web_flash_messages::FlashMessage;
 use secrecy::{ExposeSecret, Secret};
+use sqlx::PgPool;
 
 use crate::{
-    session_state::TypedSession,
-    utils::{e500, see_other},
+    authentication::{validate_credentials, AuthError, Credentials}, routes::get_username, session_state::TypedSession, utils::{e500, see_other}
 };
 
 #[derive(serde::Deserialize)]
@@ -16,18 +16,20 @@ pub struct ChangePasswordFormData {
 
 #[tracing::instrument(
     name = "Submit change password",
-    skip(session, form),
+    skip(session, form, pool),
     fields(user_id=tracing::field::Empty)
 )]
 pub async fn change_password(
     form: web::Form<ChangePasswordFormData>,
     session: TypedSession,
+    pool: web::Data<PgPool>,
 ) -> Result<HttpResponse, actix_web::Error> {
     let user_id_mb = session.get_user_id().map_err(e500)?;
-    match user_id_mb {
+    let user_id = match user_id_mb {
         None => return Ok(see_other("/login")),
         Some(user_id) => {
-            tracing::Span::current().record("user_id", &tracing::field::display(&user_id))
+            tracing::Span::current().record("user_id", &tracing::field::display(&user_id));
+            user_id
         }
     };
 
@@ -37,6 +39,23 @@ pub async fn change_password(
         )
         .send();
         return Ok(see_other("/admin/password"));
+    }
+
+    let username = get_username(user_id, &pool).await.map_err(e500)?;
+
+    let credentials = Credentials {
+        username,
+        password: form.0.current_password,
+    };
+
+    if let Err(e) = validate_credentials(credentials, &pool).await {
+        return match e {
+            AuthError::InvalidCredentials(_) => {
+                FlashMessage::error("The current password is incorrect.").send();
+                Ok(see_other("/admin/password"))
+            },
+            AuthError::UnexpectedError(_) => Err(e500(e)),
+        }
     }
 
     todo!()
