0a4683d5fdb1074c09678174cf605ffc54a29c09
Gregory Sech <sech.gregory@gmail.com>
2024-06-05 08:10:22 +0200

    green: change password implemented ref: reject anonymous users now is a function.

--- src/routes/admin/password/post.rs
+++ src/routes/admin/password/post.rs
@@ -1,7 +1,8 @@
-use actix_web::{web, HttpResponse};
+use actix_web::{error::InternalError, web, HttpResponse};
 use actix_web_flash_messages::FlashMessage;
 use secrecy::{ExposeSecret, Secret};
 use sqlx::PgPool;
+use uuid::Uuid;
 
 use crate::{
     authentication::{validate_credentials, AuthError, Credentials},
@@ -17,6 +18,18 @@ pub struct ChangePasswordFormData {
     new_password_check: Secret<String>,
 }
 
+async fn reject_anonymous_users(session: TypedSession) -> Result<Uuid, actix_web::Error> {
+    let user_id_mb = session.get_user_id().map_err(e500)?;
+    match user_id_mb {
+        Some(user_id) => Ok(user_id),
+        None => {
+            let response = see_other("/login");
+            let e = anyhow::anyhow!("The user has not logged in");
+            Err(InternalError::from_response(e, response).into())
+        },
+    }
+}
+
 #[tracing::instrument(
     name = "Submit change password",
     skip(session, form, pool),
@@ -27,14 +40,9 @@ pub async fn change_password(
     session: TypedSession,
     pool: web::Data<PgPool>,
 ) -> Result<HttpResponse, actix_web::Error> {
-    let user_id_mb = session.get_user_id().map_err(e500)?;
-    let user_id = match user_id_mb {
-        None => return Ok(see_other("/login")),
-        Some(user_id) => {
-            tracing::Span::current().record("user_id", &tracing::field::display(&user_id));
-            user_id
-        }
-    };
+    
+    let user_id = reject_anonymous_users(session).await?;
+    tracing::Span::current().record("user_id", &tracing::field::display(&user_id));
 
     if form.new_password.expose_secret() != form.new_password_check.expose_secret() {
         FlashMessage::error(
@@ -61,5 +69,7 @@ pub async fn change_password(
         };
     }
 
-    todo!()
+    crate::authentication::change_password(user_id, form.0.new_password, &pool).await.map_err(e500)?;
+    FlashMessage::error("Your password has been changed.").send();
+    Ok(see_other("/admin/password"))
 }
